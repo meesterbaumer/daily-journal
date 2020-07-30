a7c0a95e71989e62a57da9e0d216ad0fb1a8005f
Michael Baumer <meeesterbaumer@gmail.com>
2020-07-30 15:10:42 -0500

    Added beginnings of a save feature for caputiring new notes

--- scripts/Entry/journalDataProvider.js
+++ scripts/Entry/journalDataProvider.js
@@ -1,5 +1,11 @@
 let entries = []
 
+const eventHub = document.querySelector(".eventHub")
+
+const dispatchStateChangeEvent = () => {
+  eventHub.dispatchEvent(new CustomEvent("journalStateChanged"))
+}
+
 export const getEntries = () => {
   return fetch ("http://localhost:8088/entries")
     .then(res => res.json())
@@ -14,5 +20,17 @@ export const useJournalEntries = () => {
         Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
   )
   return sortedByDate
+}
+
+export const saveJournalEntry = (entryObj) => {
+  fetch("http://localhost:8088/entries", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(entryObj)
+  })
+  .then(getEntries)
+  .then(dispatchStateChangeEvent)
 }
 
